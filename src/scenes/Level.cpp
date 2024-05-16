b68aae504e01ed8232cb63b25ebf0ffcf13a4b8a
Alexander <alexander@example.com>
2024-05-16 22:11:26 +0200

    add level0

---
+++ src/scenes/Level.cpp
@@ -0,0 +1,5 @@
+//
+// Created by if23b269 on 13.05.24.
+//
+
+#include "Level.h"
---
+++ src/scenes/Level.h
@@ -0,0 +1,16 @@
+//
+// Created by if23b269 on 13.05.24.
+//
+
+#ifndef LEVEL_H
+#define LEVEL_H
+
+
+
+class Level {
+
+};
+
+
+
+#endif //LEVEL_H
