4a84477bb25b96d4289019d4e7504fac6c163112
if23b269 <163008472+if23b269@users.noreply.github.com>
2024-05-16 22:28:09 +0200

    Delete xmake.lua

--- xmake.lua
+++
@@ -1,9 +0,0 @@
-add_rules("mode.debug", "mode.release")
-
-add_requires("raylib")
-
-target("raylib-scaffold")
-    set_kind("binary")
-    add_files("src/*.cpp")
-    add_packages("raylib")
-    set_languages("c++11")
