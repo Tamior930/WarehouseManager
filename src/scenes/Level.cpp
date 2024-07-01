6027de6946e2b42b6520cc8cba3e8b1684c443c6
BSCDulak <110743178+BSCDulak@users.noreply.github.com>
2024-07-01 17:54:25 +0200

    amend add drawPlayerMovementKeyText function, text now displays in different size based on resolution.
    Took 1 hour 1 minute
    
    Took 1 minute

--- src/scenes/Level.cpp
+++ src/scenes/Level.cpp
@@ -337,34 +337,21 @@ void Level::render() {
         DrawTexture(textures.wall, wallCoord->x, wallCoord->y, RAYWHITE);
     }
     EndMode2D();
-    int _textResolution;
-    if(Options::getIndex() == 0)
-    {
-        _textResolution = 1;
-    }
-    if(Options::getIndex() == 1)
-    {
-        _textResolution = 2;
-    }
-    if(Options::getIndex() == 2)
-    {
-        _textResolution = 3;
-    }
     if (playersCoordinates.size() >= 1)
     {
-        drawPlayerMovementKeyText("PLAYER1: W/S/A/D to move",10, 10, 160, 10, 10, _textResolution);
+        drawPlayerMovementKeyText("PLAYER1: W/S/A/D to move",10, 10, 160, 10, 10, Options::getIndex()+1);
         /*DrawRectangle(10, 10, 160, 10, Fade(RAYWHITE, 0.6f));
         DrawText("PLAYER1: W/S/A/D to move", 10, 10, 10, MAROON);*/
     }
     if (playersCoordinates.size() >= 2)
     {
-        drawPlayerMovementKeyText("PLAYER2: Arrowkeys to move",10, 20, 160, 10, 10, _textResolution);
+        drawPlayerMovementKeyText("PLAYER2: Arrowkeys to move",10, 20, 160, 10, 10, Options::getIndex()+1);
         /*DrawRectangle(10, 20, 160, 10, Fade(RAYWHITE, 0.6f));
         DrawText("PLAYER2: Arrowkeys to move", 10, 20, 10, MAROON);*/
     }
     if (playersCoordinates.size() >= 3)
     {
-        drawPlayerMovementKeyText("PLAYER2: T/F/G/H to move",10, 30, 160, 10, 10, _textResolution);
+        drawPlayerMovementKeyText("PLAYER2: T/F/G/H to move",10, 30, 160, 10, 10, Options::getIndex()+1);
 
 /*        DrawRectangle(10, 30, 160, 10, Fade(RAYWHITE, 0.6f));
         DrawText("PLAYER2: T/F/G/H to move", 10, 30, 10, MAROON);*/
